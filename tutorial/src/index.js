5790ae46b084d3818c5dcdfaa1037692fcdc7a59
amit03210 <ami9cse@gmail.com>
2026-03-26 19:40:10 +0530

    update

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,1 +1,10 @@
-<html></html>
+import React from "react";
+import ReactDom from "react-dom/client";
+
+function Greeting() {
+  return <h1>My First Component</h1>;
+}
+
+const root = ReactDom.createRoot(document.getElementById("root"));
+
+root.render(<Greeting />);
